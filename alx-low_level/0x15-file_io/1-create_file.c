54207762452e15255ca190515efd4b32c6760778
Sarahgad <sarahgad91@gmail.com>
2023-07-03 22:31:23 +0300

    Add Task 1 & 2

---
+++ alx-low_level/0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "file.h"
+/**
+ *create_file - a function that creates a file. 
+ *@filename: name of file that created
+ *@text_content: the texet you write
+ *Return: 1 always success. 
+*/
+int create_file(const char *filename, char *text_content)
+{
+/*Declaration*/
+int fd;
+int writes;
+/*creating file*/
+    fd = open(filename ,  O_CREAT | O_RDONLY | O_TRUNC, 0600);
+    writes = write(fd, text_content, strlen(text_content));
+    if (fd == -1 || writes == -1)
+    {
+        return (-1);
+    }
+    close(fd);
+    return (1);
+
+    }
+
+
+
+
+
+
---
+++ alx-low_level/0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,32 @@
+#include"file.h"
+/**
+ *append_text_to_file -  a function that appends text at the end.
+ *@filename: the name of file
+ *@text_content: the content null terminated
+ *Return: 1 always success
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+/*declaration*/
+int fd, writes;
+/*Edge cases*/
+if (filename == NULL)
+    return(-1);
+    fd = open(filename, O_WRONLY|O_APPEND);
+    if (fd == -1)
+    {
+        perror("open");
+        return (-1);
+    }
+    if (text_content != NULL)
+    {
+    writes = write(fd, text_content, strlen(text_content));
+    if (writes == -1)
+    {
+        perror ("write");
+        return (-1);
+    }
+    }
+    close(fd);
+    return(1);
+}
